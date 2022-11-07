ca8aff1bcedc236cd2975477cd85464b5c8aa961
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-11-07 14:39:35 +0530

    fetch products

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: "deco",
+      slug: "nike-slim-shirt",
+      category: "decorations",
+      image: "/images/deco.jpg", // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "makeup",
+      slug: "adidas-fit-shirt",
+      category: "Shirts",
+      image: "/images/makeup.jpg",
+      price: 250,
+      countInStock: 20,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      name: "teddy",
+      slug: "nike-slim-pant",
+      category: "Pants",
+      image: "/images/teddy.jpg",
+      price: 25,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality product",
+    },
+    {
+      name: "wedding",
+      slug: "adidas-fit-pant",
+      category: "Pants",
+      image: "/images/wedding.jpg",
+      price: 65,
+      countInStock: 5,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+export default data;
