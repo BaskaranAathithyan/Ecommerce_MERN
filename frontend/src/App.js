237c54c06b4b74ee63580fb08bd181234b96fb1f
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-11-07 09:41:04 +0530

    setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Master</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
