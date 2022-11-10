e15e30ee7ce07cc5fdff4b73fedd37b5c64364cb
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-11-10 21:37:18 +0530

    add data to mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,7 +1,7 @@
 const data = {
   products: [
     {
-      _id: "1",
+      //_id: "1",
       name: "deco",
       slug: "nike-slim-shirt",
       category: "decorations",
@@ -14,7 +14,7 @@ const data = {
       description: "high quality shirt",
     },
     {
-      _id: "2",
+      //_id: "2",
       name: "makeup",
       slug: "adidas-fit-shirt",
       category: "Shirts",
@@ -27,7 +27,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "3",
+      //_id: "3",
       name: "teddy",
       slug: "nike-slim-pant",
       category: "Pants",
@@ -40,7 +40,7 @@ const data = {
       description: "high quality product",
     },
     {
-      _id: "4",
+      //_id: "4",
       name: "wedding",
       slug: "adidas-fit-pant",
       category: "Pants",
