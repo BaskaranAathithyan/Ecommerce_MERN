2d02d25f220742b10091425e381cf7291ed1d364
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-11-21 20:52:29 +0530

    signin

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -58,6 +58,7 @@ export default function SigninScreen() {
         </Form.Group>
         <div className="mb-3">
           <Button type="submit">Sign In</Button>
+          <Link to={`/cart?redirect=${redirect}`}></Link>
         </div>
         <div className="mb-3">
           New customer?{" "}
