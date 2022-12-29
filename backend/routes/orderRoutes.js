88816ba136ba57588a36af530700a778740e8c11
BaskaranAathithyan <itsthiyan11@gmail.com>
2022-12-30 00:21:36 +0530

    dashboard backend & UI

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
 import User from "../models/userModel.js";
+import Product from "../models/productModel.js";
 import { isAuth, isAdmin } from "../utils.js";
 
 const orderRouter = express.Router();
--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -7,6 +7,7 @@ import MessageBox from "../components/MessageBox";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Chart from "react-google-charts";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -20,7 +21,6 @@ const reducer = (state, action) => {
       };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
-
     default:
       return state;
   }
@@ -31,14 +31,13 @@ export default function DashboardScreen() {
     loading: true,
     error: "",
   });
-
   const { state } = useContext(Store);
   const { userInfo } = state;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data } = await axios.get(`/api/orders/summary`, {
+        const { data } = await axios.get("/api/orders/summary", {
           headers: { Authorization: `Bearer ${userInfo.token}` },
         });
         dispatch({ type: "FETCH_SUCCESS", payload: data });
@@ -58,7 +57,7 @@ export default function DashboardScreen() {
       {loading ? (
         <LoadingBox />
       ) : error ? (
-        <MessageBox varient="danger">{error}</MessageBox>
+        <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <>
           <Row>
@@ -70,37 +69,70 @@ export default function DashboardScreen() {
                       ? summary.users[0].numUsers
                       : 0}
                   </Card.Title>
-                  <Card.Text>Users</Card.Text>
+                  <Card.Text> Users</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
-
             <Col md={4}>
               <Card>
                 <Card.Body>
                   <Card.Title>
-                    {summary.orders && summary.orders[0]
+                    {summary.orders && summary.users[0]
                       ? summary.orders[0].numOrders
                       : 0}
                   </Card.Title>
-                  <Card.Text>Orders</Card.Text>
+                  <Card.Text> Orders</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
-
             <Col md={4}>
               <Card>
                 <Card.Body>
                   <Card.Title>
-                    {summary.orders && summary.orders[0]
+                    $
+                    {summary.orders && summary.users[0]
                       ? summary.orders[0].totalSales.toFixed(2)
                       : 0}
                   </Card.Title>
-                  <Card.Text>Total Sales</Card.Text>
+                  <Card.Text> Orders</Card.Text>
                 </Card.Body>
               </Card>
             </Col>
           </Row>
+          <div className="my-3">
+            <h2>Sales</h2>
+            {summary.dailyOrders.length === 0 ? (
+              <MessageBox>No Sale</MessageBox>
+            ) : (
+              <Chart
+                width="100%"
+                height="400px"
+                chartType="AreaChart"
+                loader={<div>Loading Chart...</div>}
+                data={[
+                  ["Date", "Sales"],
+                  ...summary.dailyOrders.map((x) => [x._id, x.sales]),
+                ]}
+              ></Chart>
+            )}
+          </div>
+          <div className="my-3">
+            <h2>Categories</h2>
+            {summary.productCategories.length === 0 ? (
+              <MessageBox>No Category</MessageBox>
+            ) : (
+              <Chart
+                width="100%"
+                height="400px"
+                chartType="PieChart"
+                loader={<div>Loading Chart...</div>}
+                data={[
+                  ["Category", "Products"],
+                  ...summary.productCategories.map((x) => [x._id, x.count]),
+                ]}
+              ></Chart>
+            )}
+          </div>
         </>
       )}
     </div>
